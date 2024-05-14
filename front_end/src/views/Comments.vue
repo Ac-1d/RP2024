40457130b52fb81f764c9f9aadd64e8444e0583c
李正阳 <13378255+li-zhengyang2023@user.noreply.gitee.com>
2024-05-14 21:40:59 +0800

    replying and commenting together

--- front_end/src/views/Comments.vue
+++ front_end/src/views/Comments.vue
@@ -2,33 +2,32 @@
   <div class="comments">
     <!-- 评论显示 -->
     <div v-for="(comment, index) in comments" :key="index" class="comment">
-      <h6>{{ comment.author }}:</h6>
-      <p>{{ comment.text }} 赞：{{ comment.likes }}</p>
-      <button @click="likeComment(index)">点赞</button>
-      <button @click="deleteComment(index)">删除</button>
-      <button @click="showReply(index)">评论</button>
+      <!-- 循环显示的每条评论 -->
+      <div class="single-comment">
+        <h6>{{ comment.author }}:</h6>
+        <p>{{ comment.text }} 赞：{{ comment.likes }}</p>
+        <button @click="likeComment(index)">点赞</button>
+        <button @click="deleteComment(index)">删除</button>
+        <button @click="showReply(index)">展开</button>
+        <button @click="beginReply(index)">回复</button>
+      </div>
       <!-- 回复区 -->
       <div v-if="comment.showReply" class="sub-comment">  
-        <!-- 回复框 -->
-        <form @submit.prevent="addReply(index, $event)">  
-          <textarea v-model="comment.replyText" placeholder="输入你的回复..."
-          @input="checkReplyLength"></textarea>  
-          <br/><button type="submit" :disabled="comment.replyLengthExceeded">
-            提交</button>  
-          <p>剩余</p>
-        </form>  
         <!-- 回复显示 -->
         <div v-for="(reply, replyIndex) in comment.replies" :key="replyIndex" class="reply">  
           <p>{{ reply.text }} - {{ reply.author }}</p>  
         </div>  
       </div>
     </div>
-    <!-- 评论框 -->
+    <!-- 评论、回复框 -->
     <div class="comment-box">
       <form @submit.prevent="addComment">
         <textarea v-model="newCommentText" placeholder="输入你的评论..."  
         @input="checkCommentLength"></textarea>
-        <p>剩余字数 {{ remainingRelpyText(index) }}</p>
+        <p>剩余字数 {{ remainingText }}</p>
+        <p v-if="replying">正在回复 {{ comments[replyIndex].author }}</p>
+        <!-- 返回评论 -->
+        <button v-if="replying" @click="endReply()">返回评论</button>
         <button type="submit" :disabled="commentLengthExceeded">提交</button>
       </form>
     </div>
@@ -46,7 +45,6 @@ export default {
           likes: 0, 
           showReply: false,
           replyText: '',
-          replyLengthExceeded: false,
           replies: [{text:'你好', author:'李正阳'},],
         },
         // 更多评论...  
@@ -55,6 +53,8 @@ export default {
       newCommentText: '',
       maxText: 10,
       commentLengthExceeded: false,
+      replying: false,
+      replyIndex: 0,
       myname: '匿名'
       // 当前用户名
     };
@@ -62,24 +62,30 @@ export default {
   computed: {
     remainingText(){
       return this.maxText-this.newCommentText.length;
-    },
-    remainingRelpyText(index){
-      return this.maxText-this.comments[index].newReply.length;
     }
   },
   methods: {
     addComment() {
       if (this.newCommentText) {
-        const newComment = { 
-          text: this.newCommentText, 
-          author: this.myname, 
-          showReply: false,
-          likes: 0, 
-          replyText: '',
-          replyLengthExceeded: false,
-          replies: []
-        };
-        this.comments.push(newComment);
+        if (this.replying) {
+          const newReply = {
+            text: this.newCommentText, 
+            author: this.myname
+          };
+          this.comments[this.replyIndex].replies.push(newReply);
+          this.endReply();
+        }
+        else {
+          const newComment = { 
+            text: this.newCommentText, 
+            author: this.myname, 
+            showReply: false,
+            likes: 0, 
+            replyText: '',
+            replies: []
+          };
+          this.comments.push(newComment);
+        }
         this.newCommentText = '';
       }
     },
@@ -113,10 +119,14 @@ export default {
       // 阻止表单默认的提交行为  
       event.preventDefault();  
     },
-    checkReplyLength(index) {  
-      this.comments[index].replyLengthExceeded = 
-        this.comments[index].newReply.length > this.maxText;  
-    },  // 检查评论的长度，不能超过maxText
+    beginReply(index){
+      this.replyIndex=index;
+      this.replying=true;
+    },
+    endReply(){
+      this.replyIndex=0;
+      this.replying=false;
+    }
   }
 };  
 </script>
