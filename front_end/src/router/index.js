46674cc2a74386015c8dc09df36f2d42a44f0bca
22371320 <1400456288@qq.com>
2024-05-16 19:19:43 +0800

    04/05/16郑玉完成了Home界面的分类模块

--- front_end/src/router/index.js
+++ front_end/src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
 import Home from "../views/Home.vue";
+import BookDetail from "../views/BookDetail.vue"; // 引入 BookDetail 组件
 
 Vue.use(VueRouter);
 
@@ -18,6 +19,12 @@ const routes = [
     // which is lazy-loaded when the route is visited.
     component: () =>
       import(/* webpackChunkName: "about" */ "../views/About.vue")
+  },
+  {
+    path: "/book/:bookId",
+    name: "BookDetail",
+    component: BookDetail,
+    props: true
   }
 ];
 
