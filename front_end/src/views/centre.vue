dec75b09f97f440ee998c2883b63cce252658d32
pizza2/pizza2 <22371444@buaa.edu.cn>
2024-05-19 23:19:15 +0800

    '2_lyr'

--- front_end/src/views/centre.vue
+++ front_end/src/views/centre.vue
@@ -1,61 +1,152 @@
 <template>
-  <div class="profile">
-    <div class="avatar">
-      <img src="@/assets/Avatar.jpg" alt="Avatar" width="200" height="150" />
-    </div>
-    <div class="info">
-      <h2>昵称：{{ nickname }}</h2>
-      <h2>等级：{{ level }}</h2>
-      <h2>个人签名：{{ signature }}</h2>
-    </div>
-  <button @click="showLoginModal = true">登录|注册</button>
-  <LoginModal v-if="showLoginModal" @close="showLoginModal = false" />
+  <div class="center">
+    <header class="header">
+        <div class="header-content">
+            <div class="avatar">
+                <img src="@/assets/Avatar.jpg" alt="Avatar" />
+            </div>
+            <ul class="info" >
+            <li class='NickName'><strong>昵称：</strong>{{ nickname }}</li>
+                <li class='Level'><strong>等级：</strong>lv{{ level }}</li>
+            </ul>
 
+        </div>
+    </header>
+    <header class="down">
+    <aside class="sidebar">
+      <ul class='infoSide'>
+        <li>性别：{{ sex }}</li>
+        <li>手机号：{{ hiddenTele }}</li>
+        <li>个性签名：{{ signature }}</li>
+      </ul>
+    </aside>
+    <main class="main">
+      <h2>我的书架</h2>
+      <!--
+      <ul>
+        <li v-for="(book, index) in books" :key="index">{{ book.title }}</li>
+      </ul>
+      -->
+    </main>
+    </header>
   </div>
 </template>
 
 <script>
-import LoginModal from '@/views/log.vue';
 export default {
-   components: {
+   /* components: {
       LoginModal
-   },
+   }, */
   data() {
     return {
       nickname: "John Doe",
       signature: "好好学习，天天向上!",
-      level:"lv7",
-      avatarUrl: "https://example.com/avatar.jpg", // 你的头像图片地址
-      showLoginModal: false
+      level:"7",
+      avatar: "../assets/Avatar.jpg", // 你的头像图片地址
+      sex:'男',
+      tele:'15513107588'
     };
-  }
+  },
+  computed:{
+    hiddenTele(){
+        const prefix = this.tele.substring(0, 3);
+        const suffix = this.tele.substring(this.tele.length - 2);
+        return `${prefix}****${suffix}`;
+    },
+  },
 };
-
-
-
 </script>
 
-<style>
-.profile {
+
+<style scoped>
+:root {
+  my_top: 500px;
+}
+.center {
   display: flex;
   align-items: center;
 }
 
+.header {
+
+  position:absolute;
+  top:65px;
+  left:50px;
+  right:50px;
+  height:200px;
+
+  background-size: cover;
+  background-position: top;
+  align-items: center;
+  justify-content: center;
+  background-color: #f5f5f5;
+}
+.down {
+  position:absolute;
+  top:265px;
+  left:50px;
+  right:50px;
+  height:450px;
+}
+
+.header-content {
+  text-align: left;
+  color: black;
+}
 .avatar {
   width: 100px;
   height: 100px;
   border-radius: 50%;
-  overflow: hidden;
-  margin-right: 20px;
+  object-fit: cover;
+  margin-bottom: 10px;
 }
 
 .avatar img {
-  width: 100%;
-  height: 100%;
+  position:absolute;
+  bottom:30px;
+  left:100px;
+  border-radius: 45%;
+  width: 110px;
+  height: 110px;
   object-fit: cover;
+}
+.NickName {
+    font-size:30px;
+}
+.Level {
+    font-size:20px;
+}
+.sidebar {
+  position:absolute;
+  top:0px;
+  left:0px;
+  width:200px;
+  height:100%;
+  background-color: #f5f5f5;
+}
+.infoSide{
+  list-style-type: none;
+  position:absolute;
+  top:10px;
+
+}
+.main {
+  position:absolute;
+  top:0px;
+  left:200px;
+  right:0px;
+  height:100%;
+  background-color: yellow;
 }
 
 .info {
-  font-size: 24px;
+  list-style-type: none;
+  position:absolute;
+  bottom:10px;
+  left:210px;
+}
+
+li {
+  margin-bottom: 10px;
 }
 </style>
