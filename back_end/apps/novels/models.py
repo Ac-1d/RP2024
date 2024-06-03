b468b13019c10df7172a74868b8fe7e4fbefa80e
aviator24 <14192307+Aviator24@user.noreply.gitee.com>
2024-06-03 15:43:05 +0800

    V1.3

--- back_end/apps/novels/models.py
+++ back_end/apps/novels/models.py
@@ -3,6 +3,7 @@ import os
 from django.db import models
 from ..users.models import User
 from django.conf import settings
+from django.db.models import Sum, Avg
 
 
 def novel_detail(detail):
@@ -75,9 +76,25 @@ class Author(models.Model):
     author_icon = models.FileField(upload_to='icon/', default='icon/default.jpg', verbose_name='作者图片')
     author_user = models.ForeignKey(User, null=True, related_name='author', db_constraint=False,
                                     on_delete=models.DO_NOTHING, verbose_name='是否是本网站用户')
+    # popularity = models.IntegerField(default=0, verbose_name='作者热度')
+    # average_rating = models.FloatField(default=0.0, verbose_name='平均评分')
 
     class Meta:
         verbose_name_plural = '作者表'
+
+    @property
+    def popularity(self):
+        # 计算所有关联小说的点击数之和
+        return self.Novel.aggregate(Sum('dianji')).get('dianji__sum', 0)
+
+    @property
+    def average_rating(self):
+        # 使用 double underscore (__) notation 来进行跨表查询
+        # 'Novel' 是 Author 到 Novel 的关系名（related_name='Novel'）
+        # 'Novel__comment' 是 Novel 到 Comment 的关系名
+        result = self.Novel.aggregate(avg_up_number=Avg('novel_comments__up_number'))
+        # 返回平均值，如果没有数据则返回 0.0
+        return result['avg_up_number'] or 0.0
 
 
 # 小说分类
@@ -91,7 +108,7 @@ class Novel_category(models.Model):
     @property
     def novel_list(self):
         novel_list = []
-        for i in self.novel_category.all():
+        for i in self.novel_category.all().order_by('-tuijian'):
             novel_list.append({
                 'novel_name': i.novel_name,
                 'novel_img': r'media/image/' + str(i.novel_img).split('\\')[-1],
@@ -99,7 +116,8 @@ class Novel_category(models.Model):
                 'novel_detail': novel_detail(i.detail),
                 'novel_author': i.author.author_name,
                 'novel_wroks': i.total_words,
-                'novel_status': i.novel_status
+                'novel_status': i.novel_status,
+                'tuijian':i.tuijian
             })
 
         return novel_list
@@ -110,17 +128,19 @@ class Novel_category(models.Model):
 
 # 小说章节表
 class Novel_chapter(models.Model):
+    #chapter_id = models.IntegerField(primary_key=True,default=-1)
+    chapter_id=models.IntegerField(default=-1)
     is_free = models.BooleanField(default=True, verbose_name='是否免费')
     price = models.IntegerField(verbose_name='价格', default=0)
     novel_chapter = models.CharField(max_length=64, verbose_name='小说章节')
-    content = models.CharField(verbose_name='章节路径', max_length=255)
+    content = models.FileField(upload_to='novel_content',verbose_name='章节内容', blank=True)
     words = models.IntegerField(verbose_name='字数', default=0)
     novel = models.ForeignKey(to='Novel', null=True, related_name='Novel', verbose_name='小说外键',
                               on_delete=models.CASCADE)
 
     class Meta:
         verbose_name_plural = '小说章节表'
-
+        unique_together = ('novel', 'chapter_id')  # 确保每本小说中的chapter_id是唯一的
     def __str__(self):
         return self.novel_chapter
 
@@ -147,18 +167,20 @@ class Novel_chapter(models.Model):
             return None
 
 
-# 评论点赞表
+# 评论表
 class Comment(models.Model):
     comment_time = models.DateTimeField(auto_now_add=True)  # 评论时间
     comment_content = models.TextField()  # 评论内容
-    user = models.ForeignKey(User, db_constraint=False, on_delete=models.DO_NOTHING,default=None)  # 评论用户
-    Novel = models.ForeignKey(to='Novel', db_constraint=False, on_delete=models.CASCADE)  # 评论的小说
-    up_number = models.IntegerField(default=0)  # 点赞数
-    parent = models.ForeignKey(to='Comment', db_constraint=False, null=True, on_delete=models.DO_NOTHING)  # 子评论
+    user= models.ForeignKey(User, db_constraint=False, on_delete=models.DO_NOTHING, default=None)  # 评论用户
+    novel= models.ForeignKey('Novel', related_name='novel_comments', db_constraint=False,
+                              on_delete=models.CASCADE)  # 评论的小说
+    chapter= models.ForeignKey('Novel_chapter', related_name='chapter_comments', db_constraint=False,
+                                on_delete=models.CASCADE,default=0)  # 评论的章节
+    up_number = models.IntegerField(default=0)  # 评分
+
 
     class Meta:
-        verbose_name_plural = '小说评论表'
-
+        verbose_name_plural = '评论表'
 
 # 小说书架表
 class Novel_list(models.Model):
