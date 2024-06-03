b0ef56135c0168c4d314d715e9d18dafc642c28d
李正阳 <13378255+li-zhengyang2023@user.noreply.gitee.com>
2024-06-03 11:00:06 +0800

    login

--- front_end/src/js/Api.js
+++ front_end/src/js/Api.js
@@ -1,35 +1,12 @@
 import axios from 'axios';  
-
-// 登录
-// 注意，这里如果会返回400错误，先看看用户名密码是否正确
-// 400并不是因为请求没发出去，而是验证失败
-// 只要请求的端口是8000,（后端的端口）应该就没问题
-
 // 跨域问题暂时通过代理解决了，但是部署之后还要用其他方法
-export function login(username, password) {
-  console.log('log in');
-  if (username && password) {
-    console.log({
-      username: username,  
-      password: password  
-    });
-    return axios.post("/users/login", {  
-      "username": username,  
-      "password": password  
-    })
-    .catch(function (error) {  
-      if (error.code === 'ECONNABORTED') {  
-        // 请求因超时而被中断  
-        console.error('请求超时！');  
-      } else {  
-        // 处理其他类型的错误  
-        console.error(error);  
-      }  
-    });  
-  }
-  else return 'failed!';
-}  
 
+const replyLimitTime = 5000;// 5秒响应时间
+
+// 查询用户信息
+export function userInfo() {
+
+}
 // 查找小说类别
 export function category() {
   console.log('category');
@@ -64,34 +41,3 @@ export function novelContent(novel_id, chapter_id) {
   return axios.get('/novels/chapter?id=' + novel_id + 
   '&chapter_id=' + chapter_id);
 }
-
-
-
-// export function login2() {
-//   let baseURL = 'http://127.0.0.1:8000';
-//   let ruleForm = {"username":"A", "password":"123"};
-//   console.log('log in');
-//   fetch(baseURL + '/users/login', {
-//     method: 'post',
-//     headers: {'content-type':'application/json'},
-//     body: JSON.stringify(ruleForm)
-//   }).then(async Response => {
-//     // loading = false;
-//     if (Response.ok) {
-//       return Response.json();
-//     } else {
-//       const r = await Response.json();
-//       return await Promise.reject(r);
-//     }
-//   }).then(data => {
-//     console.log('success');
-//     console.log(data);
-//   }).catch(error => {
-//     this.$message({
-//       type: 'error',
-//       message: error && error.message,
-//       showClose: true
-//     });
-//     // loading = false;
-//   })
-// }
--- front_end/src/store/index.js
+++ front_end/src/store/index.js
@@ -1,44 +1,51 @@
 import Vue from "vue";
 import Vuex from "vuex";
+import axios from 'axios';  
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {
-    isLoggedIn: false, // 初始登录状态为false 
-    username: 'lzy',
-    token: '12345678',
+    loggedIn: false,
+    userinfo: null,
   },
   mutations: {
-    LOGIN(state, payload) {  
-      // 假设payload是一个对象，包含了登录后的数据，如用户名、token等  
-      state.isLoggedIn = true;
-      // 你可以在这里设置其他登录后的状态  
-      state.username = payload.username; 
-      state.token = payload.token;
+    LOGIN(state, data) { 
+      // data是登录请求返回数据
+      state.loggedIn = true;
+      state.userinfo = data;
     },  
     LOGOUT(state) {  
-      state.isLoggedIn = false  ;
-      // 你可以在这里清除其他登录相关的状态  
-      // state.username = null  
-      // state.token = null  
+      state.loggedIn = false;
+      state.userinfo = null;
     }  
   },
   actions: {
-    login({ commit }, payload) {  
-      // 这里通常是一个异步操作，比如发送一个API请求来验证登录凭据  
-      // 假设验证成功，我们提交一个mutation来改变状态  
-      commit('LOGIN', payload);
+    async login({ commit }, loginData) {  
+      let username = loginData.username;
+      let password = loginData.password;
+      if (username && password) {
+        await axios.post("/users/login", {  
+          username: username,  
+          password: password,
+        }).then(response => {
+          if (response.data.status == 200 && response.data.msg == '登录成功') {
+            commit('LOGIN', response.data);
+            return '登录成功';
+          }
+        }).catch(error => {
+          // console.log(error.response);
+          if (error.response.status == 400) return '用户名或密码错误';
+          else return '其他错误';
+        })
+      }
     },  
-    logout({ commit }) {  
-      // 同样，这里可能包含一些清理逻辑，比如删除token等  
-      this.state.token = '';
-      // 然后我们提交一个mutation来改变状态  
+    logout({ commit }) {
       commit('LOGOUT');
     }  
   },
   getters: {
-    isLoggedIn: state => state.isLoggedIn,  
+    loggedIn: state => state.loggedIn,  
     
   },
   modules: {}
