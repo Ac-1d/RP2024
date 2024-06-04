27a28e54c068cdaa28cd8a9c796304b0aabe330e
李正阳 <13378255+li-zhengyang2023@user.noreply.gitee.com>
2024-06-04 20:08:49 +0800

    Comments complete

--- front_end/src/views/Comments.vue
+++ front_end/src/views/Comments.vue
@@ -20,15 +20,16 @@
     <!-- 评论、回复框 -->
     <el-footer style="min-height: 10vh;">
       <el-form :inline="true">
-        <el-form-item>
-          <textarea v-model="newComment.content"></textarea>
+        <el-form-item style="width:60vw;">
+          <textarea style="width: 60vw; border: 1px solid black;" v-model="newComment.content"></textarea>
         </el-form-item>
-        <el-form-item>
+        <el-form-item style="width:10vw;">
           <el-rate v-model="newComment.up_number" :colors="colors"></el-rate>
           <el-button @click="addComment" style="float: left;">发送</el-button>
         </el-form-item>
       </el-form>
     </el-footer>
+    <Login_window :dialogflag="logvisible" @closedia="closeDialog"></Login_window>
   </el-container>
 </template>
 
@@ -36,21 +37,19 @@
 import {addComments} from '@/js/Api.js';
 import {getComments} from '@/js/Api.js';
 import {extractDateTime} from '@/js/Time.js';
+import Login_window from '@/components/Login_window.vue';
 
 import '@/css/text.css';
 import '@/css/layout.css';
+
 export default {
   name: "Comments",
-
+  components: { Login_window },
   data() {
     return {
-      bookId: 2, chapterId: 1,
-      // userInfo: this.$store.state.userInfo, // 当前用户信息
+      bookId: 2, chapterId: 2,
+      
       userInfo: null,
-
-      // 评论分页功能
-      currentPage: 1, // 当前是第几页
-      commentsPerPage: 5, // 每页有五条评论
 
       comments: null,// 已有评论
 
@@ -66,31 +65,58 @@ export default {
 
       // 评级显示颜色
       colors: ['#99A9BF', '#F7BA2A', '#FF9900'],
+
+      // 显示登录界面
+      logvisible: false,
     };
   },
   async created() {// 该页面创建的时候，就通过向后端发送请求，载入评论
     // this.bookId = this.$route.params.bookId;
-    this.comments = await getComments(this.bookId, this.chapterId);
+    const respond = await getComments(this.bookId, this.chapterId);
+    this.comments = respond;
     this.newComment.novel_id = this.bookId;
     this.newComment.chapter_id = this.chapterId;
-    console.log(this.comments);
+    if(this.$store.state.loggedIn)
+      this.userInfo = this.$store.state.userInfo; // 如果登录，载入当前用户信息
+    // console.log(this.comments);
   },
   methods: {
     todate(time) {
       return extractDateTime(time);
     },
+    showLogdialog() {
+      this.logvisible = true;
+      // 可以在此处添加更多登录逻辑
+    },
+    closeDialog() {
+      this.logvisible = false;
+    },
     async addComment() {
       let newComment = this.newComment;
+      if (this.userInfo == null) {
+        this.showLogdialog();return;
+      }
       if (newComment.content == '') {
-        return;
+        this.openMsg('请留下宝贵意见');return;
       }
       if (newComment.up_number == 0) {
-        return;
+        this.openMsg('请您打分');return;
       }
       await addComments(newComment);
-      // this.comments.push(this.newComment);
-      // this.content = '';
-    }
+      await getComments(this.bookId, this.chapterId);
+      this.content = '';
+    },
+    openMsg(msg) {
+      this.$alert(msg, '提示', {
+        confirmButtonText: '确定',
+        callback: action => {
+          this.$message({
+            type: 'info',
+            message: `action: ${ action }`
+          });
+        }
+      });
+    },
   }
 };  
 </script>
